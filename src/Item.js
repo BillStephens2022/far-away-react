4b99fd83beeaaf81927ff8e8a11ebfe541244f36
BillStephens2022 <two4onebill@yahoo.com>
2024-07-08 10:35:10 -0400

    added functionality to delete items from the list

--- src/Item.js
+++ src/Item.js
@@ -1,9 +1,9 @@
-export default function Item({ item }) {
+export default function Item({ item, onDeleteItem }) {
   return (
     <li key={item.id}>
       <input type="checkbox" checked={item.packed} />
       <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description} </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
